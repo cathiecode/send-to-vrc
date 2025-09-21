2b275a5e57e3a563b564c13872835e270cec631a
cathiecode <cathiecode@gmail.com>
2025-09-21 23:02:16 +0900

    chore: download ffmpeg by npm prepare

---
+++ scripts/prepare.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import { Readable } from "stream";
+
+// You can get source codes and license text at https://github.com/cathiecode/send-to-vrc-ffmpeg/releases/download/n8.0
+const WINDOWS_FFMPEG_BINARY =
+  "https://github.com/cathiecode/send-to-vrc-ffmpeg/releases/download/n8.0/ffmpeg.exe";
+
+async function downloadFile(url, outputPath) {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error(`Failed to download ${url}: ${res.statusText}`);
+  const fileStream = fs.createWriteStream(outputPath);
+  await new Promise((resolve, reject) => {
+    const stream = Readable.from(res.body);
+    stream.pipe(fileStream);
+    stream.on("error", reject);
+    fileStream.on("finish", resolve);
+    fileStream.on("error", reject);
+  });
+}
+
+async function main() {
+  await downloadFile(WINDOWS_FFMPEG_BINARY, "./src-tauri/resources/ffmpeg.exe");
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
