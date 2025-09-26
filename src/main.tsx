dd7bd394e1e6ad4eda04883803856f30b1006e85
cathiecode <cathiecode@gmail.com>
2025-09-26 18:46:36 +0900

    fix: argument parse feature

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,33 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import { invoke } from "@tauri-apps/api/core";
+import { parseArgs } from "./args";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Suspense fallback={null}>
-      <App />
-    </Suspense>
-  </React.StrictMode>,
-);
+async function main() {
+  const args = await invoke("get_args");
+
+  const options = parseArgs(args as string[]);
+
+  if (options.mode === "send") {
+    if (!options.fileToSend) {
+      throw new Error("fileToSend is required in send mode");
+    }
+
+    if (!window.location.href.endsWith("/send")) {
+      window.location.href = "/send";
+    }
+  }
+
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <Suspense fallback={null}>
+        <App />
+      </Suspense>
+    </React.StrictMode>,
+  );
+}
+
+main().catch((err) => {
+  alert("Failed to start/ the app: " + String(err));
+});
