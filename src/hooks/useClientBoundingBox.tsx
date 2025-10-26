cc3935a418935ccf08474c709cc26dc0b92decf0
cathiecode <cathiecode@gmail.com>
2025-10-26 14:11:17 +0900

    fix: remove unused resize handler from useBoundingClientRect

--- src/hooks/useClientBoundingBox.tsx
+++ src/hooks/useClientBoundingBox.tsx
@@ -5,34 +5,6 @@ export default function useBoundingClientRect() {
 
   const [boundingClientRect, setBoundingClientRect] = useState<DOMRect>();
 
-  useLayoutEffect(() => {
-    const element = ref.current;
-
-    if (!element) {
-      return;
-    }
-
-    const handleResize = () => {
-      const rect = element.getBoundingClientRect();
-
-      setBoundingClientRect(rect);
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [ref]);
-
-  /*const effectiveScale = useMemo(() => {
-    return ref.current?.offsetWidth && boundingClientRect
-      ? ref.current.offsetWidth / boundingClientRect.width
-      : 1;
-  }, [boundingClientRect, ref]);*/
-
   const [effectiveScale, setEffectiveScale] = useState(1);
 
   useLayoutEffect(() => {
