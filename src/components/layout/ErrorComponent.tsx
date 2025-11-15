8344434c3982bac2d67ee39f84aac3bc550139a3
cathiecode <cathiecode@gmail.com>
2025-11-16 07:06:49 +0900

    fix: Something went wrong reload button

--- src/components/layout/ErrorComponent.tsx
+++ src/components/layout/ErrorComponent.tsx
@@ -5,13 +5,20 @@ type ErrorComponentProps = {
 export default function ErrorComponent(props: ErrorComponentProps) {
   const { error } = props;
 
+  const onReloadClicked = () => {
+    window.location.reload();
+  };
+
   return (
     <div>
       <h1>エラーが発生しました Something went wrong!</h1>
       Send to VRCの実行中に復帰不可能なエラーが発生しました。 An unrecoverable
       error occurred while running Send to VRC.
       <div>
-        まずは: <button>アプリケーションを再読み込みする</button>
+        まずは:
+        <button onClick={onReloadClicked}>
+          アプリケーションを再読み込みする
+        </button>
       </div>
       <div>
         直らなかったら(Does not work?):
